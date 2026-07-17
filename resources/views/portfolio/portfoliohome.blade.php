2d9a52a034c77b12fd20dc8435d1191f9f6c1e60
Najeeb Ullah <nu085051@gmail.com>
2026-07-17 21:04:09 +0300

    changed

--- resources/views/portfolio/portfoliohome.blade.php
+++ resources/views/portfolio/portfoliohome.blade.php
@@ -275,7 +275,7 @@
         <!-- Project 2 -->
         <div class="col-lg-4 col-md-6">
           <div class="project-card">
-         <img src="{{ asset('images/tailor.PNG') }}"
+         <img src="{{ asset('images/tailor.png') }}"
      alt="Tailor Management"
      class="project-image">
 
