7fec6939addaf6fbc1ced1f8d43e1af6f1dafc49
Najeeb Ullah <nu085051@gmail.com>
2026-07-17 13:41:07 +0300

    delete cdn from portfolio hom

--- resources/views/portfolio/portfoliohome.blade.php
+++ resources/views/portfolio/portfoliohome.blade.php
@@ -6,8 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@section('title') Najeeb Ullah Portfolio @endsection</title>
-    <!-- Bootstrap 5 CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  
 
     <style>
         body {
