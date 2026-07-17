46212c21e4d1e099e252408a37c1e0cafd58a87e
Najeeb Ullah <nu085051@gmail.com>
2026-07-17 11:44:35 +0300

    Update portfolio layout

--- resources/views/portfolio/portfoliolayout.blade.php
+++ resources/views/portfolio/portfoliolayout.blade.php
@@ -534,8 +534,8 @@ document.querySelectorAll('.dropdown-submenu').forEach(function(submenu) {
 
 
    <!-- Before the closing </body> tag -->
-   <script src="{{asset('js/bootstrap.bundle.min.js') }}"></script>
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> --}}
+   <!-- <script src="{{asset('js/bootstrap.bundle.min.js') }}"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> 
 @stack('script')
 
 </body>
