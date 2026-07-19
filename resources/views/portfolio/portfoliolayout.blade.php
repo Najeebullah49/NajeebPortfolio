19da060ccc2fdb2ee7e7e84c1ef139b422d789b9
Najeeb Ullah <nu085051@gmail.com>
2026-07-19 22:33:29 +0300

    change

--- resources/views/portfolio/portfoliolayout.blade.php
+++ resources/views/portfolio/portfoliolayout.blade.php
@@ -535,7 +535,7 @@ document.querySelectorAll('.dropdown-submenu').forEach(function(submenu) {
 
   
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-@stack('script')
+
 
 </body>
 </html>
